884597ef46c525b46cc8319e3a153485edd1289b
ivis-miyachi <kyosuke.miyachi@ivis.co.jp>
2025-11-11 10:20:42 +0900

    buckup

--- tools/backup.sh
+++ tools/backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 now=$(date +%Y%m%d_%H%M%S)
-docker compose exec db pg_dump -U postgres -F c -b -v -f /backups/backup_$now.dump methoddb
-docker cp $(docker compose ps -q db):/backups/backup_$now.dump ./backups/
+docker compose exec db mkdir -p /backups
+docker compose exec db pg_dump -U postgres -F c -b -v -f /backups/backup_$now.sql methoddb
+docker cp $(docker compose ps -q db):/backups/backup_$now.sql ./backups/
